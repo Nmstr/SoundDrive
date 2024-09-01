e20220b2d44054f2912f03d9f003452eaa7c5832
Nmstr <publicNamester@gmail.com>
2024-09-01 16:53:48 +0200

    force working

--- SoundDrive/main.py
+++ SoundDrive/main.py
@@ -1,8 +1,5 @@
 from MenuButton.menu_button import MenuButton
-<<<<<<< HEAD
-=======
 from SoundDriveDB import SoundDriveDB
->>>>>>> f9da3ae (basic music playback test)
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import QFile
@@ -10,10 +7,7 @@ import PySoundSphere
 import sys
 import os
 
-<<<<<<< HEAD
-=======
 MUSIC_DIR = os.path.abspath("../music")
->>>>>>> f9da3ae (basic music playback test)
 
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
@@ -30,17 +24,12 @@ class MainWindow(QMainWindow):
 
         # Create player
         self.player = PySoundSphere.AudioPlayer("pygame")
-<<<<<<< HEAD
-        self.player.load("../song.mp3")
-        self.player.volume = 0.15
-=======
         #self.player.load("../song.mp3")
         self.player.volume = 0.075
 
         # Create db
         self.db_access = SoundDriveDB()
         self.db_access.db.create_db()
->>>>>>> f9da3ae (basic music playback test)
 
         # Create menu buttons
         self.add_menu_button("home")
@@ -49,14 +38,6 @@ class MainWindow(QMainWindow):
         # Connect buttons
         self.ui.play_btn.clicked.connect(self.play)
         self.ui.stop_btn.clicked.connect(self.stop)
-<<<<<<< HEAD
-
-    def play(self):
-        self.player.play()
-
-    def stop(self):
-        self.player.stop()
-=======
         self.ui.last_btn.clicked.connect(self.last)
         self.ui.next_btn.clicked.connect(self.next)
         self.ui.add_songs_btn.clicked.connect(self.add_songs)
@@ -82,7 +63,6 @@ class MainWindow(QMainWindow):
         all_songs = os.listdir(MUSIC_DIR)
         for song in all_songs:
             self.db_access.songs.create(song, os.path.join(MUSIC_DIR, song))
->>>>>>> f9da3ae (basic music playback test)
 
     def add_menu_button(self, button_type) -> None:
         layout = self.ui.menu.layout()
