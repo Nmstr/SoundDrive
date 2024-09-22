5dfaab5be269680f1a8799fa2fe4da6238fd94f3
Nmstr <publicNamester@gmail.com>
2024-09-22 13:08:42 +0200

    songs deleted status now gets adjusted

--- SoundDrive/SoundDriveDB/songs.py
+++ SoundDrive/SoundDriveDB/songs.py
@@ -1,5 +1,6 @@
 from .db import _connect, _hash_file
 import threading
+import os
 
 def create(song_name: str, file_path: str, artist_names: str = "") -> None:
     """
@@ -26,7 +27,7 @@ def create(song_name: str, file_path: str, artist_names: str = "") -> None:
     finally:
         conn.close()
 
-def mark_as_deleted() -> None:
+def mark_as_deleted(song_id: int, value: int) -> None:
     """
     Mark song as deleted
     """
@@ -35,9 +36,9 @@ def mark_as_deleted() -> None:
     try:
         cursor.execute('''
         UPDATE songs
-        SET deleted = 1
+        SET deleted = ?
         WHERE id = ?
-        ''', (1,))
+        ''', (value, song_id,))
 
         conn.commit()
     finally:
@@ -59,6 +60,21 @@ def query() -> list:
     finally:
         conn.close()
 
+def query_all() -> list:
+    """
+    Query all songs in db (including deleted)
+    """
+    conn, cursor = _connect()
+
+    try:
+        cursor.execute('''
+        SELECT * FROM songs
+        ''')
+        songs = cursor.fetchall()
+        return songs
+    finally:
+        conn.close()
+
 def query_id(song_id: int) -> list:
     """
     Query songs in db after id
@@ -68,8 +84,7 @@ def query_id(song_id: int) -> list:
     try:
         cursor.execute('''
         SELECT * FROM songs
-        WHERE deleted = 0
-        AND id = ?
+        WHERE id = ?
         ''', (song_id,))
         song = cursor.fetchall()
         if not song:
@@ -87,8 +102,7 @@ def query_path(song_path: str) -> list:
     try:
         cursor.execute('''
         SELECT * FROM songs
-        WHERE deleted = 0
-        AND filepath = ?
+        WHERE filepath = ?
         ''', (song_path,))
         song = cursor.fetchall()
         if not song:
@@ -122,5 +136,14 @@ def hash_db():
                 hash_value = _hash_file(song[2])
                 _update_song_hash(song[0], hash_value)
 
+    def check_file_exists():
+        all_songs = query_all()
+        for song in all_songs:
+            if not os.path.isfile(song[2]):
+                mark_as_deleted(song[0], 1)
+            elif song[5] == 1:
+                mark_as_deleted(song[0], 0)
+
     hash_thread = threading.Thread(target=_do_hashing)
     hash_thread.start()
+    check_file_exists()
