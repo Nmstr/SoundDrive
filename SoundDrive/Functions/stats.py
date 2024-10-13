0d7f6b732e9ad2f88d82215e3f3e57283c5421ff
Nmstr <publicNamester@gmail.com>
2024-10-13 21:54:27 +0200

    moved retrieving of data to seperate function

--- SoundDrive/Functions/stats.py
+++ SoundDrive/Functions/stats.py
@@ -48,6 +48,26 @@ class BarChart(QWidget):
 def get_n_largest_elements(counter, n):
     return heapq.nlargest(n, counter.items(), key=lambda item: item[1])
 
+def get_n_most_played_songs(history_data: str, n: int, parent: object) -> tuple[list, int]:
+    """
+    Gets the n most played songs from the history data
+    :param history_data: The history data
+    :param n: The amount of songs to retrieve
+    :param parent: The parent
+    :return: The set to display and the highest value
+    """
+    songs = [song[1] for song in history_data]  # Extract song IDs from history data
+
+    song_counter = Counter(songs)
+    largest_elements = get_n_largest_elements(song_counter, n)
+    elements_set = []
+    for song, count in largest_elements:
+        song_data = parent.db_access.songs.query_id(song)
+        bar_set = QBarSet(song_data[1])
+        bar_set.append(count)
+        elements_set.append(bar_set)
+    return elements_set, max(song_counter.values())
+
 def setup_page(parent) -> None:
     """
     Adds all the data to the stats page
@@ -61,29 +81,20 @@ def setup_page(parent) -> None:
     parent.ui.usage_times_end.setDate(current_date)
 
     history_data = parent.db_access.stats.get_history()
-    songs = [song[1] for song in history_data]  # Extract song IDs from history data
 
-    song_counter = Counter(songs)
-    largest_elements = get_n_largest_elements(song_counter, 10)
-    sets = []
-    for song, count in largest_elements:
-        song_data = parent.db_access.songs.query_id(song)
-        bar_set = QBarSet(song_data[1])
-        bar_set.append(count)
-        sets.append(bar_set)
-
-    played_song_chart = BarChart("Played Songs", sets, max(song_counter.values()))
+    sets, highest_val = get_n_most_played_songs(history_data, 10, parent)
+    played_song_chart = BarChart("Played Songs", sets, highest_val)
     layout = parent.clear_field(parent.ui.played_songs_container, QVBoxLayout, amount_left = 0)
     layout.addWidget(played_song_chart)
 
-    played_artists_chart = BarChart("Played Artists", sets, max(song_counter.values()))
+    played_artists_chart = BarChart("Played Artists", sets, highest_val)
     layout = parent.clear_field(parent.ui.played_artists_container, QVBoxLayout, amount_left = 0)
     layout.addWidget(played_artists_chart)
 
-    occurrence_chart = BarChart("Song Occurrences", sets, max(song_counter.values()))
+    occurrence_chart = BarChart("Song Occurrences", sets, highest_val)
     layout = parent.clear_field(parent.ui.occurrence_container, QVBoxLayout, amount_left = 0)
     layout.addWidget(occurrence_chart)
 
-    usage_times_chart = BarChart("Usage Times", sets, max(song_counter.values()))
+    usage_times_chart = BarChart("Usage Times", sets, highest_val)
     layout = parent.clear_field(parent.ui.usage_times_container, QVBoxLayout, amount_left = 0)
     layout.addWidget(usage_times_chart)
